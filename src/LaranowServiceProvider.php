03924d9632ced54078c517dea9b461a4dc8ecb40
oceantech <leonard13396@hotmail.com>
2020-05-17 00:18:07 +0800

    new test

---
+++ src/LaranowServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Leonard133\Laranow;
+
+use App\Console\Commands\PackageCommand;
+use Illuminate\Support\ServiceProvider;
+
+class LaranowServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                AuthCommand::class,
+                LaranowCommand::class,
+                PackageCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        PresetCommand::macro();
+    }
+}
